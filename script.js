dfd2c7ef092a89e96fb00f70b19f234c8424deed
utkarshrpathak <utkarsh.unv.ngo@gmail.com>
2026-07-01 10:05:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// Smooth fade animation
 
+const cards = document.querySelectorAll('.card,.stat-card');
+
+const observer = new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity="1";
+
+entry.target.style.transform="translateY(0px)";
+
+}
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+
+card.style.transform="translateY(40px)";
+
+card.style.transition="0.8s";
+
+observer.observe(card);
+
+});
